02a1ae78d9c5f9efab8aa55955d20fe96a05840e
Ada Bardız <adabardiz@icloud.com>
2026-07-30 20:19:32 +0300

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,13 +1,10 @@
-
 import random
 import string
 
 chars = " " + string.punctuation + string.digits + string.ascii_letters
 chars = list(chars)
 key = chars.copy()
-
 random.shuffle(key)
-
 plain_text = input("enter a message to encrypt: ")
 cipher_text = ""
 
